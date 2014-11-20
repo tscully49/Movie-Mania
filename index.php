b90fab7390a2cb4277b3114fe714bbf3d62aac39
tscully49 <thomas1374@yahoo.com>
2014-11-20 11:28:04 -0600

    Changed all names to Movie-Mania on each page

--- index.php
+++ index.php
@@ -267,39 +267,13 @@
                                     <div class="col-xs-3">
                                         <i class="fa fa-film fa-5x"></i>
                                     </div>
-                                    <?PHP
-                                        include("database.php");
-                                        $conn=pg_connect(HOST. " ".DBNAME." ".USERNAME." ".PASSWORD); // Connects to the database
-                                 
-                                        if(!$conn){
-                                            echo"<p> Connection Fail</p>";
-                                        }
-
-                                        $result = pg_prepare($conn, "movies",'SELECT count(*) AS count FROM movie');
-                                        $result = pg_execute($conn, "movies", array());
-                                        $years = pg_prepare($conn, "year", 'SELECT (max(year)-min(year)) AS total FROM movie');
-                                        $years = pg_execute($conn, "year", array());
-                                        $actors = pg_prepare($conn, "actor", 'SELECT count(*) FROM actor');
-                                        $actors = pg_execute($conn, "actor", array());
-                                        $genres = pg_prepare($conn, "genre", 'SELECT count(*) FROM genre');
-                                        $genres = pg_execute($conn, "genre", array());
-
-                                        $num_movies=pg_fetch_array($result, null, PGSQL_ASSOC);
-                                        $num = $num_movies['count'];
-                                        $num_years = pg_fetch_array($years, null, PGSQL_ASSOC);
-                                        $num_years2 = $num_years['total'];
-                                        $num_actors = pg_fetch_array($actors, null, PGSQL_ASSOC);
-                                        $num_actors2 = $num_actors['count'];
-                                        $num_genres = pg_fetch_array($genres, null, PGSQL_ASSOC);
-                                        $num_genres2 = $num_genres['count'];
-
-                                        echo"<div class='col-xs-9 text-right'>";
-                                        echo"\n\t<div class='huge'>$num</div>";
-                                        echo"\n\t<div>Movies</div>";
-                                        echo"</div>";
-
-                                        pg_close($conn);
-                                    ?>
+                                        <div class='col-xs-9 text-right'>
+                                        <?PHP
+                                            require("index_queries.php");
+                                            echo"\n<div class='huge'>$num</div>";
+                                        ?>
+                                            <div>Movies</div>
+                                        </div>
                                     <!--<div class="col-xs-9 text-right">
                                         <div class="huge">9234</div>
                                         <div>Movies</div>
---
+++ index_queries.php
@@ -0,0 +1,28 @@
+<?PHP
+	 include("database.php");
+    $conn=pg_connect(HOST. " ".DBNAME." ".USERNAME." ".PASSWORD); // Connects to the database
+
+    if(!$conn){
+        echo"<p> Connection Fail</p>";
+    }
+
+    $result = pg_prepare($conn, "movies",'SELECT count(*) AS count FROM movie');
+    $result = pg_execute($conn, "movies", array());
+    $years = pg_prepare($conn, "year", 'SELECT (max(year)-min(year)) AS total FROM movie');
+    $years = pg_execute($conn, "year", array());
+    $actors = pg_prepare($conn, "actor", 'SELECT count(*) FROM actor');
+    $actors = pg_execute($conn, "actor", array());
+    $genres = pg_prepare($conn, "genre", 'SELECT count(*) FROM genre');
+    $genres = pg_execute($conn, "genre", array());
+
+    $num_movies=pg_fetch_array($result, null, PGSQL_ASSOC);
+    $num = $num_movies['count'];
+    $num_years = pg_fetch_array($years, null, PGSQL_ASSOC);
+    $num_years2 = $num_years['total'];
+    $num_actors = pg_fetch_array($actors, null, PGSQL_ASSOC);
+    $num_actors2 = $num_actors['count'];
+    $num_genres = pg_fetch_array($genres, null, PGSQL_ASSOC);
+    $num_genres2 = $num_genres['count'];
+
+    pg_close($conn);
+?>

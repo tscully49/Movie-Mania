e0615e54402cd35949c23345e69b777b9be7cadf
tscully49 <thomas1374@yahoo.com>
2014-11-23 16:06:48 -0600

    Added a dynamic total Box Office Chart to the index page which reference database and pulls the 10 top total grossed movies of all time

--- index.php
+++ index.php
@@ -426,7 +426,7 @@
                                         <span class="badge">$38053000</span>
                                         <i class="fa fa-fw fa-video-camera"></i> 
                                         <?PHP 
-                                            echo "\n$new[movies][title]";
+                                            echo "\narray(new[movies][title])";
                                         ?>
                                     </a>
                                     <a href="#" class="list-group-item">

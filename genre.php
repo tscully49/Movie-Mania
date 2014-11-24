6a69da5fa6ebc84cf1b7965c26fb6cffc1eae1f3
tscully49 <thomas1374@yahoo.com>
2014-11-24 12:34:29 -0600

    Added every genre page so users can find all movies by a certain genre

--- genre.php
+++ genre.php
@@ -207,65 +207,20 @@
                     </div>
                 </div>
                 <!-- /.row -->
-
+                
                 <div class="row">
-                    <div class="col-lg-6">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <?PHP
-                                    if ($_GET['genre'] != NULL) {
-                                        $type = $_GET['genre'];
-                                        echo"<h3 class='panel-title'><i class='fa fa-tasks fa-fw'></i><strong> Every $type Movie</strong></h3>";
-                                    }
-                                    else {
-                                        $type = "All";
-                                        echo"<h3 class='panel-title'><i class='fa fa-tasks fa-fw'></i><strong> $type Movies</strong></h3>"; 
-                                    }
-                                ?>
-                            </div>
-                            <div class="panel-body">
-                                <div class="table-responsive">
-                                    <table class="table table-bordered table-hover table-striped">
-                                        <thead>
-                                            <tr>
-                                                <?PHP
-                                                    include("../secure/database.php");
-                                                    $conn=pg_connect(HOST. " ".DBNAME." ".USERNAME." ".PASSWORD); // Connects to the database
+                    <!-- Decides whether all the genres should be printed out or if just a single one should by checking the get parameters-->
+                    <?PHP
+                        include("php-functions.php");
+                        if ($_GET['genre'] == "All") {
+                            print_all_queries();
+                        }
+                        else {
+                            print_single_genre($_GET['genre']);
+                        }
+                    ?>
+                    <!-- Prints out either all genres or just the specified one -->
 
-                                                    if(!$conn){
-                                                        echo"<p> Connection Fail</p>";
-                                                    }
-
-                                                    $this_genre = pg_prepare($conn, "genre_query", 'WITH id_list AS (SELECT movie_id FROM movie_genre as mg INNER JOIN genre as g ON (mg.genre_id = g.genre_id) WHERE g.genre = $1) SELECT title FROM movie INNER JOIN id_list ON (id_list.movie_id = movie.id) ORDER BY title ASC');
-                                                    $this_genre = pg_execute($conn, "genre_query", array($type));
-
-                                                    $num_fields = pg_num_fields($this_genre);
-                                                    for ($i=0;$i<$num_fields;$i++) { // Prints out all headers for the fields 
-                                                        $fieldName = pg_field_name($this_genre, $i);
-                                                        echo "\t\t\n<th>$fieldName</th>"; 
-                                                    }
-                                                ?>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        <?PHP
-                                            while ($all_genres = pg_fetch_array($this_genre, null, PGSQL_ASSOC)) {
-                                                echo"<tr>";
-                                                foreach($all_genres as $col) { // Prints out all the info 
-                                                    echo"\n\t\t<td>$col</td>";
-                                                }
-                                                echo"\n\t</tr>";
-                                            }
-                                        ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            	<div class="text-right">
-                                    <a href="#">View All Movies <i class="fa fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div class="col-lg-5">
                         <div class="panel panel-default">
                             <div class="panel-heading">
--- php-funcs.php
+++ php-funcs.php
@@ -61,5 +61,117 @@ function printActorTable($result)
 	echo "</table>\n";
 }
 
+function print_all_genres() {
+
+    include("../secure/database.php");
+    $conn=pg_connect(HOST. " ".DBNAME." ".USERNAME." ".PASSWORD); // Connects to the database
+
+    if(!$conn){
+        echo"<p> Connection Fail</p>";
+    }
+
+    $this_genre = pg_prepare($conn, "genre_query", 'SELECT genre FROM genre');
+    $this_genre = pg_execute($conn, "genre_query", array());
+
+    $all_genres = pg_fetch_array($this_genre, null, PGSQL_ASSOC)
+
+    foreach ($all_genres) {
+		echo"\n<div class='col-lg-3'>";
+		    echo"\n\t<div class='panel panel-default'>";
+		        echo"\n\t\t<div class='panel-heading'>";
+		            echo"\n\t\t\t<h3 class='panel-title'><i class='fa fa-tasks fa-fw'></i><strong> $all_genres Movies</strong></h3>"; 
+		        echo"\n\t\t</div>";
+		        echo"\n\t\t<div class='panel-body'>";
+		            echo"\n\t\t\t<div class='table-responsive'>";
+		                echo"\n\t\t\t\t<table class='table table-bordered table-hover table-striped'>";
+		                    echo"\n\t\t\t\t\t<thead>";
+		                        echo"\n\t\t\t\t\t\t<tr>";
+
+		                                $num_fields = pg_num_fields($this_genre);
+		                                for ($i=0;$i<$num_fields;$i++) { // Prints out all headers for the fields 
+		                                    $fieldName = pg_field_name($this_genre, $i);
+		                                    echo "\n\t\t\t\t\t\t\t<th>$fieldName</th>"; 
+		                                }
+
+		                        echo"\n\t\t\t\t\t\t</tr>";
+		                    echo"\n\t\t\t\t\t</thead>";
+		                    echo"\n\t\t\t\t\t<tbody>";
+
+		                        while ($all_genres = pg_fetch_array($this_genre, null, PGSQL_ASSOC)) {
+		                            echo"<tr>";
+		                            foreach($all_genres as $col) { // Prints out all the info 
+		                                echo"\n\t\t<td>$col</td>";
+		                            }
+		                            echo"\n\t</tr>";
+		                        }
+
+		                    echo"\n\t\t\t\t\t</tbody>";
+		                echo"\n\t\t\t\t</table>";
+		            echo"\n\t\t\t</div>";
+		        	echo"\n\t\t\t<div class='text-right'>";
+		                echo"\n\t\t\t\t<a href='#'>View All $all_genres Movies <i class='fa fa-arrow-circle-right'></i></a>";
+		            echo"\n\t\t\t</div>";
+		        echo"\n\t\t\t</div>";
+		    echo"\n\t\t</div>";
+		echo"\n\t</div>";
+	}
+}
+
+function print_single_genre($genre) {
+	<div class="col-lg-6">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <?PHP
+                    if ($_GET['genre'] != NULL) {
+                        $type = $_GET['genre'];
+                        echo"<h3 class='panel-title'><i class='fa fa-tasks fa-fw'></i><strong> Every $type Movie</strong></h3>";
+                    }
+                    else {
+                        $type = "All";
+                        echo"<h3 class='panel-title'><i class='fa fa-tasks fa-fw'></i><strong> $type Movies</strong></h3>"; 
+                    }
+                ?>
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <?PHP
+                                    include("../secure/database.php");
+                                    $conn=pg_connect(HOST. " ".DBNAME." ".USERNAME." ".PASSWORD); // Connects to the database
+                                    if(!$conn){
+                                        echo"<p> Connection Fail</p>";
+                                    }
+                                    $this_genre = pg_prepare($conn, "genre_query", 'WITH id_list AS (SELECT movie_id FROM movie_genre as mg INNER JOIN genre as g ON (mg.genre_id = g.genre_id) WHERE g.genre = $1) SELECT title FROM movie INNER JOIN id_list ON (id_list.movie_id = movie.id) ORDER BY title ASC LIMIT 10');
+                                    $this_genre = pg_execute($conn, "genre_query", array($type));
+                                    $num_fields = pg_num_fields($this_genre);
+                                    for ($i=0;$i<$num_fields;$i++) { // Prints out all headers for the fields 
+                                        $fieldName = pg_field_name($this_genre, $i);
+                                        echo "\t\t\n<th>$fieldName</th>"; 
+                                    }
+                                ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?PHP
+                            while ($all_genres = pg_fetch_array($this_genre, null, PGSQL_ASSOC)) {
+                                echo"<tr>";
+                                foreach($all_genres as $col) { // Prints out all the info 
+                                    echo"\n\t\t<td>$col</td>";
+                                }
+                                echo"\n\t</tr>";
+                            }
+                        ?>
+                        </tbody>
+                    </table>
+                </div>
+            	<div class="text-right">
+                    <a href="#">View All Movies <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+}
 
 ?>

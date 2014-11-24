f3d63c787efd69799879fb1600754e00cd98b9ab
tscully49 <thomas1374@yahoo.com>
2014-11-24 12:55:55 -0600

    Fixed problems with genre page when user selects All Genres

--- php-funcs.php
+++ php-funcs.php
@@ -71,19 +71,18 @@ function print_all_genres() { // Functions which prints out a table for each gen
         echo"<p> Connection Fail</p>";
     }
 
-    $this_genre = pg_prepare($conn, "genre_query", 'SELECT genre FROM genre');
-    $this_genre = pg_execute($conn, "genre_query", array());
+    $all_genres = pg_prepare($conn, "genre_query", 'SELECT genre FROM genre');
+    $all_genres = pg_execute($conn, "genre_query", array());
 
-    $all_genres = pg_fetch_array($this_genre, null, PGSQL_ASSOC)
 
-    foreach ($all_genres AS $one) {
-    	$genres = pg_prepare($conn, "genre_search", 'WITH id_list AS (SELECT movie_id FROM movie_genre as mg INNER JOIN genre as g ON (mg.genre_id = g.genre_id) WHERE g.genre = $1) SELECT title FROM movie INNER JOIN id_list ON (id_list.movie_id = movie.id) ORDER BY title ASC LIMIT 10');
-    	$genres = pg_execute($conn, "genre_search", array($one));
+    while ($one_genre = pg_fetch_array($all_genres, null, PGSQL_ASSOC)) {
+    	$this_genre = pg_prepare($conn, "genre_search", 'WITH id_list AS (SELECT movie_id FROM movie_genre as mg INNER JOIN genre as g ON (mg.genre_id = g.genre_id) WHERE g.genre = $1) SELECT title FROM movie INNER JOIN id_list ON (id_list.movie_id = movie.id) ORDER BY title ASC LIMIT 10');
+    	$this_genre = pg_execute($conn, "genre_search", array($one_genre));
 
 		echo"\n<div class='col-lg-3'>";
 		    echo"\n\t<div class='panel panel-default'>";
 		        echo"\n\t\t<div class='panel-heading'>";
-		            echo"\n\t\t\t<h3 class='panel-title'><i class='fa fa-tasks fa-fw'></i><strong> $all_genres Movies</strong></h3>"; 
+		            echo"\n\t\t\t<h3 class='panel-title'><i class='fa fa-tasks fa-fw'></i><strong> $one_genre Movies</strong></h3>"; 
 		        echo"\n\t\t</div>";
 		        echo"\n\t\t<div class='panel-body'>";
 		            echo"\n\t\t\t<div class='table-responsive'>";
@@ -91,9 +90,9 @@ function print_all_genres() { // Functions which prints out a table for each gen
 		                    echo"\n\t\t\t\t\t<thead>";
 		                        echo"\n\t\t\t\t\t\t<tr>";
 
-		                                $num_fields = pg_num_fields($genres);
+		                                $num_fields = pg_num_fields($this_genre);
 		                                for ($i=0;$i<$num_fields;$i++) { // Prints out all headers for the fields 
-		                                    $fieldName = pg_field_name($genres, $i);
+		                                    $fieldName = pg_field_name($this_genre, $i);
 		                                    echo "\n\t\t\t\t\t\t\t<th>$fieldName</th>"; 
 		                                }
 
@@ -101,9 +100,9 @@ function print_all_genres() { // Functions which prints out a table for each gen
 		                    echo"\n\t\t\t\t\t</thead>";
 		                    echo"\n\t\t\t\t\t<tbody>";
 
-		                        while ($all_genres = pg_fetch_array($genres, null, PGSQL_ASSOC)) {
+		                        while ($info = pg_fetch_array($this_genre, null, PGSQL_ASSOC)) {
 		                            echo"<tr>";
-		                            foreach($all_genres as $col) { // Prints out all the info 
+		                            foreach($info as $col) { // Prints out all the info 
 		                                echo"\n\t\t<td>$col</td>";
 		                            }
 		                            echo"\n\t</tr>";
@@ -113,7 +112,7 @@ function print_all_genres() { // Functions which prints out a table for each gen
 		                echo"\n\t\t\t\t</table>";
 		            echo"\n\t\t\t</div>";
 		        	echo"\n\t\t\t<div class='text-right'>";
-		                echo"\n\t\t\t\t<a href='#'>View All $all_genres Movies <i class='fa fa-arrow-circle-right'></i></a>";
+		                echo"\n\t\t\t\t<a href='#'>View All $one_genre Movies <i class='fa fa-arrow-circle-right'></i></a>";
 		            echo"\n\t\t\t</div>";
 		        echo"\n\t\t\t</div>";
 		    echo"\n\t\t</div>";

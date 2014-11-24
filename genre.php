e60a0c530693058f7fd2a80c06568b2cd4a2c1ca
tscully49 <thomas1374@yahoo.com>
2014-11-24 14:56:12 -0600

    Fixed problems with genre page when user selects All Genres

--- genre.php
+++ genre.php
@@ -187,7 +187,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Search any by Genre! <small>The Genres for all of our movies!</small>
+                            Search any Movie by Genre! <small>The Genres for all of our movies!</small>
                         </h1>
 
                         <ol class="breadcrumb">
--- php-funcs.php
+++ php-funcs.php
@@ -101,7 +101,7 @@ function print_all_genres() { // Functions which prints out a table for each gen
 
 		                        while ($info = pg_fetch_array($this_genre, null, PGSQL_ASSOC)) {
 		                            echo"\n\t\t\t\t\t\t<tr>";
-		                            echo"\n\t\t\t\t\t\t<a href=movie_profile?title=$movies[title] class='list-group-item'>";
+		                            echo"\n\t\t\t\t\t\t<a href=movie_profile?title=$info[title] class='list-group-item'>";
 		                            foreach($info as $col) { // Prints out all the info 
 		                                echo"\n\t\t\t\t\t\t\t<span>$col</span>";
 		                            }

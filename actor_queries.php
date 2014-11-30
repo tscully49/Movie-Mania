fa6dfafb38d9d3920277b9296df51f6adf56fdac
tscully49 <thomas1374@yahoo.com>
2014-11-30 16:27:17 -0600

    fixed tables in the movies.php and actors.php page

--- actor_queries.php
+++ actor_queries.php
@@ -9,7 +9,7 @@
     $pop_actors = pg_prepare($conn, "Actors_query", 'SELECT name AS "Name", count(movie_id) AS "# of Movies" FROM actor_in_movie AS am INNER JOIN actor AS a ON (a.id = am.actor_id) GROUP BY actor_id, name ORDER BY count(movie_id) DESC LIMIT 20');
     $pop_actors = pg_execute($conn, "Actors_query", array());
 
-    $pop_directors = pg_prepare($conn, "directors", 'SELECT name, count(movie_id) FROM director_of_movie AS am INNER JOIN director AS a ON (a.id = am.director_id) GROUP BY director_id, name ORDER BY count(movie_id) DESC LIMIT 20 OFFSET 1');
+    $pop_directors = pg_prepare($conn, "directors", 'SELECT name as "Director Name", count(movie_id) AS "# of Movies" FROM director_of_movie AS am INNER JOIN director AS a ON (a.id = am.director_id) GROUP BY director_id, name ORDER BY count(movie_id) DESC LIMIT 20 OFFSET 1');
     $pop_directors = pg_execute($conn, "directors", array());
 
     pg_close($conn);

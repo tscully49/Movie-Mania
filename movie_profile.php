074f255302523c3f1c00297f0c988816cb9f4332
tscully49 <thomas1374@yahoo.com>
2014-12-01 08:14:27 -0600

    Changed Movie Profile pages

--- movie_profile.php
+++ movie_profile.php
@@ -48,6 +48,9 @@
     	.movie_titles {
     		display: inline;
     	}
+        td {
+            white-space: nowrap;
+        }
     </style>
 </head>
 
@@ -287,26 +290,35 @@
            while($line = pg_fetch_array($result1,null,PGSQL_ASSOC)){
                 foreach($line as $col_value){
                     $fieldname = pg_field_name($result1, $i);
-                    if ($fieldname == 'mpaa_rating') {
-                        echo "\t\t<tr><td>MPAA Rating</td><td>$col_value</td></tr>\n";
-                    }
-                    if ($fieldname == "release_date") {
-                        echo "\t\t<tr><td>Release Date</td><td>$col_value</td></tr>\n";
-                    }
-                    if ($fieldname == "domestic_gross") {
-                        echo "\t\t<tr><td>Domestic Gross</td><td>$col_value</td></tr>\n";
-                    }
-                    if ($fieldname == "rt_critic") {
-                        echo "\t\t<tr><td>Critic Rating</td><td>$col_value</td></tr>\n";
-                    }
-                    if ($fieldname == "rt_audience") {
-                        echo "\t\t<tr><td>Audience Rating</td><td>$col_value</td></tr>\n";
-                    }
-                    if ($fieldname == "imdb") {
-                        echo "\t\t<tr><td>IMDB Rating</td><td>$col_value</td></tr>\n";
-                    }
-                    else {
-                       echo "\t\t<tr><td>$fieldname</td><td>$col_value</td></tr>\n";
+                    switch ($fieldname) {
+                        case 'mpaa_rating': {
+                            echo "\t\t<tr><td>MPAA Rating</td><td>$col_value</td></tr>\n";
+                            break;
+                        }
+                        case "release_date": {
+                            echo "\t\t<tr><td>Release Date</td><td>$col_value</td></tr>\n";
+                            break;
+                        }
+                        case "domestic_gross": {
+                            echo "\t\t<tr><td>Domestic Gross</td><td>$col_value</td></tr>\n";
+                            break;
+                        }
+                        case "rt_critic": {
+                            echo "\t\t<tr><td>Critic Rating</td><td>$col_value</td></tr>\n";
+                            break;
+                        }
+                        case "rt_audience": {
+                            echo "\t\t<tr><td>Audience Rating</td><td>$col_value</td></tr>\n";
+                            break;
+                        }
+                        case "imdb": {
+                            echo "\t\t<tr><td>IMDB Rating</td><td>$col_value</td></tr>\n";
+                            break;
+                        }
+                        default : {
+                           echo "\t\t<tr><td>$fieldname</td><td>$col_value</td></tr>\n";
+                           break;
+                        }
                     }
                     $i=$i+1;
                 }

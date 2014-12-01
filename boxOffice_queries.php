dfb9bec9316de2bec2cfd88f6a6cd2ffc6fbddcb
tscully49 <thomas1374@yahoo.com>
2014-12-01 00:17:25 -0600

    Added a table to the Box Office Page

--- boxOffice_queries.php
+++ boxOffice_queries.php
@@ -12,7 +12,7 @@
     $Box_genres = pg_prepare($conn, "genre_yrs", 'WITH id_list AS (SELECT movie_genre.movie_id, genre.genre FROM movie_genre INNER JOIN genre ON (movie_genre.genre_id = genre.genre_id)) SELECT genre AS "Genre", sum(domestic_gross) AS "Total Gross" FROM movie AS m INNER JOIN id_list AS i ON (m.id = i.movie_id) GROUP BY genre ORDER BY genre ASC');
     $Box_genres = pg_execute($conn, "genre_yrs", array());
 
-    $Box_directors = pg_prepare($conn, "dir_years", 'WITH id_list AS (SELECT name, movie_id FROM director_of_movie INNER JOIN director ON (director.id = director_of_movie.movie_id)) SELECT name AS "Name", sum(domestic_gross) AS "Total Gross" FROM movie AS m INNER JOIN id_list AS i ON (m.id = i.movie_id) GROUP BY name ORDER BY name ASC');
+    $Box_directors = pg_prepare($conn, "dir_years", 'WITH id_list AS (SELECT name, movie_id FROM director_of_movie INNER JOIN director ON (director.id = director_of_movie.movie_id)) SELECT name AS "Name", sum(domestic_gross) AS "Total Gross" FROM movie AS m INNER JOIN id_list AS i ON (m.id = i.movie_id) GROUP BY name ORDER BY (sum(domestic_gross)) ASC LIMIT 20');
     $Box_directors = pg_execute($conn, "dir_years", array());
 
     pg_close($conn);

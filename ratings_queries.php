0d4e6987663663f9ab8b8ee9d57ab31b7eec05b1
tscully49 <thomas1374@yahoo.com>
2014-12-09 18:15:24 -0600

    Fixed errors with connections because of leaked information

--- ratings_queries.php
+++ ratings_queries.php
@@ -9,7 +9,7 @@
     $result1 = pg_prepare($conn, "rt_by_genre",'WITH id_list AS (SELECT genre, movie_id FROM genre INNER JOIN movie_genre ON (genre.genre_id = movie_genre.genre_id)) SELECT genre AS "Genre", round(avg(rt_critic),2) AS "Avg Critic Rating", round(avg(rt_audience),2) AS "Avg Audience Rating" FROM movie as m INNER JOIN id_list as i ON (m.id = i.movie_id) GROUP BY genre ORDER BY genre ASC');
     $result1 = pg_execute($conn, "rt_by_genre", array());
 
-    $result2 = pg_prepare($conn, "rt_by_year", 'SELECT year AS "Year", round(avg(rt_critic),2) AS "Average Critic Rating" FROM movie GROUP BY year ORDER BY year ASC');
+    $result2 = pg_prepare($conn, "rt_by_year", 'SELECT year AS "Year", round(avg(rt_critic),2) AS "Avg Critic Rating" FROM movie GROUP BY year ORDER BY year ASC');
     $result2 = pg_execute($conn, "rt_by_year", array());
 
     $result3 = pg_prepare($conn, "rt_by_director", 'WITH id_list AS (SELECT name, movie_id FROM director INNER JOIN director_of_movie ON (director.id = director_of_movie.director_id)) SELECT name AS "Name", round(avg(rt_critic),2) AS "Avg Critic Rating", count(title) AS "# of Movies Made" FROM movie as m INNER JOIN id_list as i ON (m.id = i.movie_id) GROUP BY name HAVING (count(title) > 10) ORDER BY avg(rt_critic) DESC LIMIT 20');
